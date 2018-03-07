4efebbf92fa5ea250ffca2ce148c2d0f5ce5369d
Кирилл Хмельницкий <the.kiritos@gmail.com>
2018-03-07 11:10:42 +0300

    init

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true
+  },
+  extends: ['airbnb-base', 'prettier', 'plugin:vue/recommended', "plugin:jest/recommended"],
+  // check if imports actually resolve
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: 'build/webpack.base.conf.js'
+      }
+    }
+  },
+  // add your custom rules here
+  rules: {
+    // don't require .vue extension when importing
+    'import/extensions': [
+      'error',
+      'always',
+      {
+        js: 'never',
+        vue: 'never'
+      }
+    ],
+    // allow optionalDependencies
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        optionalDependencies: ['test/unit/index.js']
+      }
+    ],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-param-reassign': 'off'
+  }
+};
